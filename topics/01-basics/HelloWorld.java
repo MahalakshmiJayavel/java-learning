f8c62216897244674475e3690be18d53894dd7d4
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-03-27 23:07:55 -0400

    updated hello world

--- topics/01-basics/HelloWorld.java
+++ topics/01-basics/HelloWorld.java
@@ -2,7 +2,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // Print a message to the console
-        //dummy comment
         System.out.println("Hello, World!");
     }
 }
