8d8191611badf5377d2ac4147be3b711da2d5b18
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-04-01 16:49:06 -0400

    Completed 10 Streams and Lambdas

---
+++ topics/10-StreamsAndLambdas/SimpleLambdaExpression.java
@@ -0,0 +1,14 @@
+public class SimpleLambdaExpression {
+    public static void main(String[] args) {
+        
+        AddNumbers add = (a, b) -> a + b;
+
+        int result = add.sum(5, 10);
+        System.out.println("sum = " + result);
+    }
+
+    @FunctionalInterface
+    interface AddNumbers {
+        int sum(int a, int b); 
+    }
+}
